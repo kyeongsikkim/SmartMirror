953e3c11265966a3da622d1261f22a7d7c1c62ab
602 <jyk602222@naver.com>
2017-08-18 15:02:49 +0900

    Add Calendar
    Calendar (Init, Next, Prev, Specific)

--- SmartMirrorWebProject/src/main/webapp/WEB-INF/views/calendar.jsp
+++ SmartMirrorWebProject/src/main/webapp/WEB-INF/views/calendar.jsp
@@ -1,8 +1,24 @@
-<%@page contentType="text/html; charset=UTF-8"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<div class='wrap'>
-	<div class='btn-holder'>
-		<span id='currentDate'></span>
+<html>
+
+  <head>
+   	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/demo.css"/>
+    <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/theme2.css"/>
+    <meta charset='utf-8' />
+  </head>
+  
+  <body>
+  <div class='wrap'>
+		<div class='btn-holder'>
+			<span id='currentDate'></span>
+		</div>
+		<div id="calendar"></div> 
 	</div>
-	<div id="calendar"></div>
-</div>
+  
+    <script type="text/javascript" src="<%=application.getContextPath()%>/resources/js/calendar.js"></script>
+   
+  </body>
+  
+</html>
--- SmartMirrorWebProject/src/main/webapp/resources/js/calendar.js
+++ SmartMirrorWebProject/src/main/webapp/resources/js/calendar.js
@@ -1,116 +1,400 @@
-var calendar;
+var count = 0;
+var Calendar = function(model, options, date) {
+	// Default Values
+	this.Options = {
+		Color : '',
+		LinkColor : '',
+		NavShow : true,
+		NavVertical : false,
+		NavLocation : '',
+		DateTimeShow : true,
+		DateTimeFormat : 'mmm, yyyy',
+		DatetimeLocation : '',
+		EventClick : '',
+		EventTargetWholeDay : false,
+		DisabledDays : [],
+		ModelChange : model
+	};
+	// Overwriting default values
+	for ( var key in options) {
+		this.Options[key] = typeof options[key] == 'string' ? options[key]
+				.toLowerCase() : options[key];
+	}
 
-function initCalendar(){
-	calendar = new controller(); 
-	calendar.init();
+	model ? this.Model = model : this.Model = {};
+	this.Today = new Date();
+
+	this.Selected = this.Today
+	this.Today.Month = this.Today.getMonth();
+	this.Today.Year = this.Today.getFullYear();
+	if (date) {
+		this.Selected = date
+	}
+	this.Selected.Month = this.Selected.getMonth();
+	this.Selected.Year = this.Selected.getFullYear();
+
+	this.Selected.Days = new Date(this.Selected.Year,
+			(this.Selected.Month + 1), 0).getDate();
+	this.Selected.FirstDay = new Date(this.Selected.Year,
+			(this.Selected.Month), 1).getDay();
+	this.Selected.LastDay = new Date(this.Selected.Year,
+			(this.Selected.Month + 1), 0).getDay();
+
+	this.Prev = new Date(this.Selected.Year, (this.Selected.Month - 1), 1);
+	if (this.Selected.Month == 0) {
+		this.Prev = new Date(this.Selected.Year - 1, 11, 1);
+	}
+	this.Prev.Days = new Date(this.Prev.getFullYear(),
+			(this.Prev.getMonth() + 1), 0).getDate();
 };
 
-function nextCalendar() {
-	calendar.onNextCalendar();
-}
-
-function prevCalendar() {
-	calendar.onPrevCalendar();
-}
-
-function specificCalendar(month) {
-	calendar.onSpecificCalendar(month);
-}
-
-function controller(target) {
-
-	var that = this;   
-	var m_oMonth = new Date();
-	m_oMonth.setDate(1);
-
-	this.init = function() {
-		that.renderCalendar();
-		that.initEvent();
-	}
-
-    /* 달력 UI 생성 */
-	this.renderCalendar = function() {
-		var arrTable = [];
-
-		arrTable.push('<table><colgroup>');
-		for(var i=0; i<7; i++) {
-			arrTable.push('<col width="100">');
-		}		
-		arrTable.push('</colgroup><thead><tr>');
-
-		var arrWeek = "일월화수목금토".split("");
-
-		for(var i=0, len=arrWeek.length; i<len; i++) {
-			var sClass = '';
-			sClass += i % 7 == 0 ? 'sun' : '';
-			sClass += i % 7 == 6 ? 'sat' : '';
-			arrTable.push('<td class="'+sClass+'">' + arrWeek[i] + '</td>');
-		}
-		arrTable.push('</tr></thead>');
-		arrTable.push('<tbody>');
-
-		var oStartDt = new Date(m_oMonth.getTime());
-        // 1일에서 1일의 요일을 빼면 그 주 첫번째 날이 나온다.
-		oStartDt.setDate(oStartDt.getDate() - oStartDt.getDay());
-
-		for(var i=0; i<100; i++) {
-			if(i % 7 == 0) {
-				arrTable.push('<tr>');
+function createCalendar(calendar, element, adjuster, specific) {
+	if (typeof specific !== 'undefined') {
+		var newDate = new Date(calendar.Today.Year, specific - 1, 1);
+		calendar = new Calendar(calendar.Model, calendar.Options, newDate);
+		element.innerHTML = '';
+	} else {
+		if (typeof adjuster !== 'undefined') {
+			if (adjuster == 0) {
+				var newDate = new Date(calendar.Today.Year,
+						calendar.Today.Month + adjuster, 1);
+				calendar = new Calendar(calendar.Model, calendar.Options,
+						newDate);
+				element.innerHTML = '';
+			} else {
+				var newDate = new Date(calendar.Selected.Year,
+						calendar.Selected.Month + adjuster, 1);
+				calendar = new Calendar(calendar.Model, calendar.Options,
+						newDate);
+				element.innerHTML = '';
 			}
+		} else {
+			for ( var key in calendar.Options) {
+				typeof calendar.Options[key] != 'function'
+						&& typeof calendar.Options[key] != 'object'
+						&& calendar.Options[key] ? element.className += " "
+						+ key + "-" + calendar.Options[key] : 0;
+			}
+		}
+	}
+	var months = [ "January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December" ];
 
-			var sClass = 'date-cell '
-            sClass += m_oMonth.getMonth() != oStartDt.getMonth() ? 'not-this-month ' : '';
-			sClass += i % 7 == 0 ? 'sun' : '';
-			sClass += i % 7 == 6 ? 'sat' : '';
+	function AddSidebar() {
+		var sidebar = document.createElement('div');
+		sidebar.className += 'cld-sidebar';
 
-			arrTable.push('<td class="'+sClass+'">' + oStartDt.getDate() + '</td>');
-			oStartDt.setDate(oStartDt.getDate() + 1);
+		var monthList = document.createElement('ul');
+		monthList.className += 'cld-monthList';
 
-			if(i % 7 == 6) {
-				arrTable.push('</tr>');
-				if(m_oMonth.getMonth() != oStartDt.getMonth()) {
-					break;
+		for (var i = 0; i < months.length - 3; i++) {
+			var x = document.createElement('li');
+			x.className += 'cld-month';
+			var n = i - (4 - calendar.Selected.Month);
+			// Account for overflowing month values
+			if (n < 0) {
+				n += 12;
+			} else if (n > 11) {
+				n -= 12;
+			}
+			// Add Appropriate Class
+			if (i == 0) {
+				x.className += ' cld-rwd cld-nav';
+				x
+						.addEventListener(
+								'click',
+								function() {
+									typeof calendar.Options.ModelChange == 'function' ? calendar.Model = calendar.Options
+											.ModelChange()
+											: calendar.Model = calendar.Options.ModelChange;
+									createCalendar(calendar, element, -1);
+								});
+				x.innerHTML += '<svg height="15" width="15" viewBox="0 0 100 75" fill="rgba(255,255,255,0.5)"><polyline points="0,75 100,75 50,0"></polyline></svg>';
+			} else if (i == months.length - 4) {
+				x.className += ' cld-fwd cld-nav';
+				x
+						.addEventListener(
+								'click',
+								function() {
+									typeof calendar.Options.ModelChange == 'function' ? calendar.Model = calendar.Options
+											.ModelChange()
+											: calendar.Model = calendar.Options.ModelChange;
+									createCalendar(calendar, element, 1);
+								});
+				x.innerHTML += '<svg height="15" width="15" viewBox="0 0 100 75" fill="rgba(255,255,255,0.5)"><polyline points="0,0 100,0 50,75"></polyline></svg>';
+			} else {
+				if (i < 4) {
+					x.className += ' cld-pre';
+				} else if (i > 4) {
+					x.className += ' cld-post';
+				} else {
+					x.className += ' cld-curr';
+				}
+
+				(function() {
+					var adj = (i - 4);
+					x
+							.addEventListener(
+									'click',
+									function() {
+										typeof calendar.Options.ModelChange == 'function' ? calendar.Model = calendar.Options
+												.ModelChange()
+												: calendar.Model = calendar.Options.ModelChange;
+										createCalendar(calendar, element, adj);
+									});
+					x.setAttribute('style', 'opacity:'
+							+ (1 - Math.abs(adj) / 4));
+					x.innerHTML += months[n].substr(0, 3);
+				}()); // immediate invocation
+
+				if (n == 0) {
+					var y = document.createElement('li');
+					y.className += 'cld-year';
+					if (i < 5) {
+						y.innerHTML += calendar.Selected.Year;
+					} else {
+						y.innerHTML += calendar.Selected.Year + 1;
+					}
+					monthList.appendChild(y);
 				}
 			}
+			monthList.appendChild(x);
 		}
-		arrTable.push('</tbody></table>');
-
-		$('#calendar').html(arrTable.join(""));
-
-		that.changeMonth();
+		sidebar.appendChild(monthList);
+		if (calendar.Options.NavLocation) {
+			document.getElementById(calendar.Options.NavLocation).innerHTML = "";
+			document.getElementById(calendar.Options.NavLocation).appendChild(
+					sidebar);
+		} else {
+			element.appendChild(sidebar);
+		}
 	}
 
-    /* Next, Prev 버튼 이벤트 */
-	this.initEvent = function() {
-		$('#btnPrev').click(that.onPrevCalendar);
-		$('#btnNext').click(that.onNextCalendar);
+	var mainSection = document.createElement('div');
+	mainSection.className += "cld-main";
+
+	function AddDateTime() {
+		var datetime = document.createElement('div');
+		datetime.className += "cld-datetime";
+
+		if (calendar.Options.NavShow && !calendar.Options.NavVertical) {
+			var rwd = document.createElement('div');
+			rwd.className += " cld-rwd cld-nav";
+			rwd.addEventListener('click', function() {
+				 prevCalendar();
+			});
+			rwd.innerHTML = '<svg height="15" width="15" viewBox="0 0 75 100" fill="rgba(0,0,0,0.5)"><polyline points="0,50 75,0 75,100"></polyline></svg>';
+			datetime.appendChild(rwd);
+		}
+
+		var today = document.createElement('div');
+		today.className += ' today';
+		today.innerHTML = months[calendar.Selected.Month] + ", "
+				+ calendar.Selected.Year;
+		datetime.appendChild(today);
+		if (calendar.Options.NavShow && !calendar.Options.NavVertical) {
+			var fwd = document.createElement('div');
+			fwd.className += " cld-fwd cld-nav";
+
+			fwd.addEventListener('click', function() {
+				nextCalendar();
+			});
+			fwd.innerHTML = '<svg height="15" width="15" viewBox="0 0 75 100" fill="rgba(0,0,0,0.5)"><polyline points="0,0 75,50 0,100"></polyline></svg>';
+			datetime.appendChild(fwd);
+		}
+
+		if (calendar.Options.DatetimeLocation) {
+			document.getElementById(calendar.Options.DatetimeLocation).innerHTML = "";
+			document.getElementById(calendar.Options.DatetimeLocation)
+					.appendChild(datetime);
+		} else {
+			mainSection.appendChild(datetime);
+		}
 	}
 
-    /* 이전 달력 */
-	this.onPrevCalendar = function() {
-		m_oMonth.setMonth(m_oMonth.getMonth() - 1);
-		that.renderCalendar();
+	function AddLabels() {
+		var labels = document.createElement('ul');
+		labels.className = 'cld-labels';
+		var labelsList = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+		for (var i = 0; i < labelsList.length; i++) {
+			var label = document.createElement('li');
+			label.className += "cld-label";
+			label.innerHTML = labelsList[i];
+			labels.appendChild(label);
+		}
+		mainSection.appendChild(labels);
+	}
+	function AddDays() {
+		// Create Number Element
+		function DayNumber(n) {
+			var number = document.createElement('p');
+			number.className += "cld-number";
+			number.innerHTML += n;
+			return number;
+		}
+		var days = document.createElement('ul');
+		days.className += "cld-days";
+		// Previous Month's Days
+		for (var i = 0; i < (calendar.Selected.FirstDay); i++) {
+			var day = document.createElement('li');
+			day.className += "cld-day prevMonth";
+			// Disabled Days
+			var d = i % 7;
+			for (var q = 0; q < calendar.Options.DisabledDays.length; q++) {
+				if (d == calendar.Options.DisabledDays[q]) {
+					day.className += " disableDay";
+				}
+			}
+
+			var number = DayNumber((calendar.Prev.Days - calendar.Selected.FirstDay)
+					+ (i + 1));
+			day.appendChild(number);
+
+			days.appendChild(day);
+		}
+		// Current Month's Days
+		for (var i = 0; i < calendar.Selected.Days; i++) {
+			var day = document.createElement('li');
+			day.className += "cld-day currMonth";
+			// Disabled Days
+			var d = (i + calendar.Selected.FirstDay) % 7;
+			for (var q = 0; q < calendar.Options.DisabledDays.length; q++) {
+				if (d == calendar.Options.DisabledDays[q]) {
+					day.className += " disableDay";
+				}
+			}
+			var number = DayNumber(i + 1);
+			// Check Date against Event Dates
+			for (var n = 0; n < calendar.Model.length; n++) {
+				var evDate = calendar.Model[n].Date;
+				var toDate = new Date(calendar.Selected.Year,
+						calendar.Selected.Month, (i + 1));
+				if (evDate.getTime() == toDate.getTime()) {
+					number.className += " eventday";
+					var title = document.createElement('span');
+					title.className += "cld-title";
+					if (typeof calendar.Model[n].Link == 'function'
+							|| calendar.Options.EventClick) {
+						var a = document.createElement('a');
+						a.setAttribute('href', '#');
+						a.innerHTML += calendar.Model[n].Title;
+						if (calendar.Options.EventClick) {
+							var z = calendar.Model[n].Link;
+							if (typeof calendar.Model[n].Link != 'string') {
+								a.addEventListener('click',
+										calendar.Options.EventClick.bind.apply(
+												calendar.Options.EventClick,
+												[ null ].concat(z)));
+								if (calendar.Options.EventTargetWholeDay) {
+									day.className += " clickable";
+									day
+											.addEventListener(
+													'click',
+													calendar.Options.EventClick.bind
+															.apply(
+																	calendar.Options.EventClick,
+																	[ null ]
+																			.concat(z)));
+								}
+							} else {
+								a.addEventListener('click',
+										calendar.Options.EventClick.bind(null,
+												z));
+								if (calendar.Options.EventTargetWholeDay) {
+									day.className += " clickable";
+									day.addEventListener('click',
+											calendar.Options.EventClick.bind(
+													null, z));
+								}
+							}
+						} else {
+							a.addEventListener('click', calendar.Model[n].Link);
+							if (calendar.Options.EventTargetWholeDay) {
+								day.className += " clickable";
+								day.addEventListener('click',
+										calendar.Model[n].Link);
+							}
+						}
+						title.appendChild(a);
+					} else {
+						title.innerHTML += '<a href="' + calendar.Model[n].Link
+								+ '">' + calendar.Model[n].Title + '</a>';
+					}
+					number.appendChild(title);
+				}
+			}
+			day.appendChild(number);
+			// If Today..
+			if ((i + 1) == calendar.Today.getDate()
+					&& calendar.Selected.Month == calendar.Today.Month
+					&& calendar.Selected.Year == calendar.Today.Year) {
+				day.className += " today";
+			}
+			days.appendChild(day);
+		}
+		// Next Month's Days
+		// Always same amount of days in calander
+		var extraDays = 13;
+		if (days.children.length > 35) {
+			extraDays = 6;
+		} else if (days.children.length < 29) {
+			extraDays = 20;
+		}
+
+		for (var i = 0; i < (extraDays - calendar.Selected.LastDay); i++) {
+			var day = document.createElement('li');
+			day.className += "cld-day nextMonth";
+			// Disabled Days
+			var d = (i + calendar.Selected.LastDay + 1) % 7;
+			for (var q = 0; q < calendar.Options.DisabledDays.length; q++) {
+				if (d == calendar.Options.DisabledDays[q]) {
+					day.className += " disableDay";
+				}
+			}
+
+			var number = DayNumber(i + 1);
+			day.appendChild(number);
+
+			days.appendChild(day);
+		}
+		mainSection.appendChild(days);
+	}
+	if (calendar.Options.Color) {
+		mainSection.innerHTML += '<style>.cld-main{color:'
+				+ calendar.Options.Color + ';}</style>';
+	}
+	if (calendar.Options.LinkColor) {
+		mainSection.innerHTML += '<style>.cld-title a{color:'
+				+ calendar.Options.LinkColor + ';}</style>';
+	}
+	element.appendChild(mainSection);
+
+	if (calendar.Options.NavShow && calendar.Options.NavVertical) {
+		AddSidebar();
+	}
+	if (calendar.Options.DateTimeShow) {
+		AddDateTime();
+	}
+	AddLabels();
+	AddDays();
+
+	function initCalendar() {
+		createCalendar(calendar, element, 0);
 	}
 
-    /* 다음 달력 */
-	this.onNextCalendar = function() {
-		m_oMonth.setMonth(m_oMonth.getMonth() + 1);
-		that.renderCalendar();
-	}
-	
-	/* 0월 달력 */
-	this.onSpecificCalendar = function(month) {
-		m_oMonth.setMonth(month-1);
-		that.renderCalendar();
+	function nextCalendar() {
+		createCalendar(calendar, element, 1);
 	}
 
-    /* 달력 이동되면 상단에 현재 년 월 다시 표시 */
-	this.changeMonth = function() {
-		$('#currentDate').text(that.getYearMonth(m_oMonth).substr(0,9));
+	function prevCalendar() {
+		createCalendar(calendar, element, -1);
 	}
 
-    /* 날짜 객체를 년 월 문자 형식으로 변환 */
-	this.getYearMonth = function(oDate) {
-		return oDate.getFullYear() + '년 ' + (oDate.getMonth() + 1) + '월';
+	function specificCalendar(month) {
+		createCalendar(calendar, element, 0, month);
 	}
 }
+
+var element = document.getElementById('caleandar');
+var obj = new Calendar();
+createCalendar(obj, element);
