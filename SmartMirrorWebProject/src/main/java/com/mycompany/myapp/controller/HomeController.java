6e6b7737a08469da549d909c53731e07ab87cb7f
Dahye Lee <PrayAnne@idahyeui-MacBook-Pro.local>
2017-08-21 12:05:05 +0900

    8/21 weather & youtube modify

--- SmartMirrorWebProject/src/main/java/com/mycompany/myapp/controller/HomeController.java
+++ SmartMirrorWebProject/src/main/java/com/mycompany/myapp/controller/HomeController.java
@@ -1,40 +1,24 @@
 package com.mycompany.myapp.controller;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.FlickrApi;
+import org.scribe.model.Token;
+import org.scribe.oauth.OAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
+import org.zalando.stups.tokens.AccessToken;
 
 import com.flickr4java.flickr.FlickrException;
 import com.github.dvdme.ForecastIOLib.FIOCurrently;
@@ -48,9 +32,6 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
-import com.mycompany.myapp.dto.Music;
-import com.mycompany.myapp.dto.Photo;
-import com.mycompany.myapp.service.Service;
 import com.mycompany.myapp.util.Feed;
 import com.mycompany.myapp.util.FeedMessage;
 import com.mycompany.myapp.util.RSSFeedParser;
@@ -60,30 +41,37 @@ public class HomeController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 	private List<FeedMessage> list;
+<<<<<<< HEAD
+	
+=======
 	private List<Music> musicList;
 	//////////////////////////////////////////////////////////////////////
 	private String fileName;
 	private String filePath;
 	/////////////////////////////////////////////////////////////////////////
+<<<<<<< Updated upstream
 
 
 	private String apiKey = "04af17713fb5285e5352234c38f805b1";
+=======
+>>>>>>> Stashed changes
 
-	GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyDoP8zx7GCoyI0BQixAfm-HGzsMldgk6kY").build();
 
-	@Autowired
-	private Service service;
-	@Autowired
-	private ServletContext servletContext;
+>>>>>>> 5e5635d42212c89653e1de1ab0608f4ffb0949a9
+	private String apiKey = "04af17713fb5285e5352234c38f805b1";
+	
+	GeoApiContext context = new GeoApiContext.Builder()
+			.apiKey("AIzaSyDoP8zx7GCoyI0BQixAfm-HGzsMldgk6kY")
+			.build();
 
 	@RequestMapping("/")
-	public String home() throws IOException {
+	public String home() {
 		return "main";
 	}
 
-	@RequestMapping("/calendar")
-	public String calendar() {
-		return "calendar";
+	@RequestMapping("/calander")
+	public String calander() {
+		return "calander";
 	}
 
 	@RequestMapping("/map")
@@ -91,131 +79,19 @@ public class HomeController {
 		return "map";
 	}
 
-	@RequestMapping("/command")
-	public String command() {
-		return "command";
-	}
-
 	@RequestMapping("/camera")
 	public String camera() {
 		return "camera";
 	}
-
-	@RequestMapping("/snapshot")
-	public void snapshot(HttpServletResponse response) throws Exception {
-		URL url = new URL("http://localhost:50001/?action=snapshot");
-		filePath = servletContext.getRealPath("/resources/photo/");
-		fileName = new Date().toString() + ".jpg";
-		
-		Photo photo = new Photo();
-		photo.setPfilename(fileName);
-		photo.setPfilepath("/SmartMirrorWebProject/resources/photo/" + fileName);
-		
-		InputStream is = new BufferedInputStream(url.openStream());
-		FileOutputStream fos = new FileOutputStream(filePath + "/" + fileName);
-		
-		int readBytes = -1;
-		byte[] cbuf = new byte[1024];
-		while(true) {
-			readBytes = is.read(cbuf);
-			if(readBytes == -1) break;
-			fos.write(cbuf, 0, readBytes);
-		}
-		
-		is.close();
-		fos.close();
-		
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("filename", fileName);
-		String json = jsonObject.toString();
-
-		response.setContentType("application/json; charset=UTF-8");
-		PrintWriter pw = response.getWriter();
-		pw.write(json);
-		pw.flush();
-		pw.close();
-		
-		service.photoUpload(photo);
-	}
-
-	@RequestMapping("/snapshotdisplay")
-	public void snapshotdisplay(String filename, HttpServletResponse response, @RequestHeader("User-Agent") String userAgent) throws Exception {
-		String encodingFileName;
-		if(userAgent.contains("MSIE") || userAgent.contains("Trident") || userAgent.contains("Edge")) {
-			encodingFileName = URLEncoder.encode(filename, "UTF-8");
-		} else {
-			encodingFileName = new String(filename.getBytes("UTF-8"), "ISO-8859-1");					
-		}
-		
-		response.addHeader("Content-Disposition", "attachment; filename=\"" + encodingFileName + "\"");
-		response.addHeader("Content-Type", "image/jpeg");
-		
-		File file = new File(filePath + "/" + filename);
-		long fileSize = file.length();
-		response.addHeader("Content-Length", String.valueOf(fileSize));
-		
-		OutputStream os = response.getOutputStream();
-		FileInputStream fis = new FileInputStream(file);
-		FileCopyUtils.copy(fis, os);
-		os.flush();
-		fis.close();
-		os.close();
-	}
-
-	@RequestMapping("/audio")
-	public String audio() {
-		return "audio";
-	}
-
-	@RequestMapping("/musicfile")
-	public void musicFile(MultipartFile attach, HttpServletResponse response) throws Exception {
-		Music music = new Music();
-
-		String filename = attach.getOriginalFilename();
-		music.setMfilename(filename);
-		music.setMfilepath("/SmartMirrorWebProject/resources/music/" + filename);
-
-		String realPath = servletContext.getRealPath("/resources/music/");
-		File file = new File(realPath + "/" + filename);
-
-		attach.transferTo(file);
-
-		service.musicUpload(music);
-	}
-
-	@RequestMapping("/musiclist")
-	public void musicList(HttpServletResponse response) throws Exception {
-		if (musicList != null) {
-			musicList.clear();
-		}
-
-		musicList = service.getMusicList();
-
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("musicList", musicList);
-		String json = jsonObject.toString();
-
-		response.setContentType("application/json; charset=UTF-8");
-		PrintWriter pw = response.getWriter();
-		pw.write(json);
-		pw.flush();
-		pw.close();
+	
+	@RequestMapping("/weatherDefault")
+	public String weatherDefault() {
+		return "weatherdefault";
 	}
 	
-	@RequestMapping("/getid")
-	public void getId(String name, String id, HttpServletResponse response) throws IOException {
-		LOGGER.info(name);
-		LOGGER.info(id);
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("name", name);
-		jsonObject.put("id", id);
-		String json = jsonObject.toString();
-
-		response.setContentType("application/json; charset=UTF-8");
-		PrintWriter pw = response.getWriter();
-		pw.write(json);
-		pw.flush();
-		pw.close();
+	@RequestMapping("/weather_View")
+	public String weather_View() {
+		return "weather";
 	}
 
 	@RequestMapping("/news")
@@ -241,14 +117,14 @@ public class HomeController {
 
 	@RequestMapping("/weather")
 	public void weather(HttpServletResponse response) throws IOException, ApiException, InterruptedException {
-
+		
 		JSONObject jsonObject = new JSONObject();
 
 		GeocodingResult[] results = GeocodingApi.geocode(context, "서울").await();
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String latitude = gson.toJson(results[0].geometry.location.lat);
 		String longitude = gson.toJson(results[0].geometry.location.lng);
-
+		
 		ForecastIO fio = new ForecastIO(apiKey);
 		fio.setUnits(ForecastIO.UNITS_SI);
 		fio.setLang(ForecastIO.LANG_ENGLISH);
@@ -346,13 +222,13 @@ public class HomeController {
 			iconW[i] = daily.getDay(i).icon().substring(1, daily.getDay(i).icon().length() - 1);
 		}
 
-		jsonObject.put("temp", temp);
-		jsonObject.put("icon", iconR);
+		jsonObject.put("temp", (int)temp);
+		jsonObject.put("icon",	iconR);
 		jsonObject.put("summary", summary);
 
 		for (int i = 1; i < 7; i++) {
-			jsonObject.put("tempMax" + i, tempMax[i]);
-			jsonObject.put("tempMin" + i, tempMin[i]);
+			jsonObject.put("tempMax" + i, (int)tempMax[i]);
+			jsonObject.put("tempMin" + i, (int)tempMin[i]);
 			jsonObject.put("iconW" + i, iconW[i]);
 			jsonObject.put("week" + i, weekList[i]);
 		}
@@ -365,12 +241,7 @@ public class HomeController {
 		pw.flush();
 		pw.close();
 	}
-
-	@RequestMapping("/weather_View")
-	public String weather_View() {
-		return "weather";
-	}
-
+	
 	@RequestMapping("/youtubevideolist")
 	public String youtube() {
 		return "youtubeplaylist";
@@ -380,17 +251,16 @@ public class HomeController {
 	public String video() {
 		return "video";
 	}
-
+	
 	@RequestMapping("/weather_detail")
-	public String weather_detail(String address, HttpServletResponse response)
-			throws URISyntaxException, FlickrException, ApiException, InterruptedException, IOException {
-
+	public void weather_detail(String address, HttpServletResponse response) throws URISyntaxException, FlickrException, ApiException, InterruptedException, IOException {
+		
 		JSONObject jsonObject = new JSONObject();
 		GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String lat = gson.toJson(results[0].geometry.location.lat);
 		String lng = gson.toJson(results[0].geometry.location.lng);
-
+		
 		ForecastIO fio = new ForecastIO(apiKey);
 		fio.setUnits(ForecastIO.UNITS_SI);
 		fio.setLang(ForecastIO.LANG_ENGLISH);
@@ -402,18 +272,111 @@ public class HomeController {
 		double temp = fdp.temperature();
 		String icon = fdp.icon();
 		String summary = hourly.getSummary();
+		double wind = fdp.windSpeed();
+		double precip = fdp.precipProbability();
+		
 		String iconR = icon.substring(1, icon.length() - 1);
-		// double tempMax = fdp.temperatureMax();
-		// double tempMin = fdp.temperatureMin();
 
-		jsonObject.put("temp", temp);
+		FIODaily daily = new FIODaily(fio);
+		double[] tempMax = new double[8];
+		double[] tempMin = new double[8];
+		String[] iconW = new String[8];
+
+		Calendar cal = Calendar.getInstance();
+		String[] weekList = new String[7];
+		int now = cal.get(Calendar.DAY_OF_WEEK);
+		switch (now) {
+		case Calendar.MONDAY:
+			weekList[0] = "Mon";
+			weekList[1] = "Tue";
+			weekList[2] = "Wed";
+			weekList[3] = "Thu";
+			weekList[4] = "Fri";
+			weekList[5] = "Sat";
+			weekList[6] = "Sun";
+			break;
+		case Calendar.TUESDAY:
+			weekList[0] = "Tue";
+			weekList[1] = "Wed";
+			weekList[2] = "Thu";
+			weekList[3] = "Fri";
+			weekList[4] = "Sat";
+			weekList[5] = "Sun";
+			weekList[6] = "Mon";
+			break;
+		case Calendar.WEDNESDAY:
+			weekList[0] = "Wed";
+			weekList[1] = "Thu";
+			weekList[2] = "Fri";
+			weekList[3] = "Sat";
+			weekList[4] = "Sun";
+			weekList[5] = "Mon";
+			weekList[6] = "Tue";
+			break;
+		case Calendar.THURSDAY:
+			weekList[0] = "Thu";
+			weekList[1] = "Fri";
+			weekList[2] = "Sat";
+			weekList[3] = "Sun";
+			weekList[4] = "Mon";
+			weekList[5] = "Tue";
+			weekList[6] = "Wed";
+			break;
+		case Calendar.FRIDAY:
+			weekList[0] = "Fri";
+			weekList[1] = "Sat";
+			weekList[2] = "Sun";
+			weekList[3] = "Mon";
+			weekList[4] = "Tue";
+			weekList[5] = "Wed";
+			weekList[6] = "Thu";
+			break;
+		case Calendar.SATURDAY:
+			weekList[0] = "Sat";
+			weekList[1] = "Sun";
+			weekList[2] = "Mon";
+			weekList[3] = "Tue";
+			weekList[4] = "Wed";
+			weekList[5] = "Thu";
+			weekList[6] = "Fri";
+			break;
+		case Calendar.SUNDAY:
+			weekList[0] = "Sun";
+			weekList[1] = "Mon";
+			weekList[2] = "Tue";
+			weekList[3] = "Wed";
+			weekList[4] = "Thu";
+			weekList[5] = "Fri";
+			weekList[6] = "Sat";
+			break;
+		}
+
+		if (daily.days() < 0) {
+			jsonObject.put("icon", "No data available");
+		}
+		
+		for (int i = 0; i < daily.days(); i++) {
+			daily.getDay(i).setTimezone("Asia/Seoul");
+			tempMax[i] = daily.getDay(i).temperatureMax();
+			tempMin[i] = daily.getDay(i).temperatureMin();
+			iconW[i] = daily.getDay(i).icon().substring(1, daily.getDay(i).icon().length() - 1);
+		}
+		
+		jsonObject.put("temp", (int)temp);
 		jsonObject.put("icon", iconR);
 		jsonObject.put("summary", summary);
-		// jsonObject.put("tempMax", String.valueOf(tempMax));
-		// jsonObject.put("tempMin", String.valueOf(tempMin));
 		jsonObject.put("latitude", lat);
 		jsonObject.put("longitude", lng);
-
+		jsonObject.put("wind", wind);
+		jsonObject.put("precip", precip*100);
+		
+		for (int i = 1; i < 7; i++) {
+			jsonObject.put("tempMax" + i, (int)tempMax[i]);
+			jsonObject.put("tempMin" + i, (int)tempMin[i]);
+			jsonObject.put("iconW" + i, iconW[i]);
+			jsonObject.put("week" + i, weekList[i]);
+		}
+		
 		String json = jsonObject.toString();
 
 		response.setContentType("application/json; charset=UTF-8");
@@ -421,6 +384,9 @@ public class HomeController {
 		pw.write(json);
 		pw.flush();
 		pw.close();
+<<<<<<< HEAD
+	}
+=======
 
 		LOGGER.info(lat);
 		LOGGER.info(lng);
@@ -681,4 +647,8 @@ public class HomeController {
 	}
 
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> 5e5635d42212c89653e1de1ab0608f4ffb0949a9
+>>>>>>> Stashed changes
 }
--- SmartMirrorWebProject/src/main/webapp/WEB-INF/views/video.jsp
+++ SmartMirrorWebProject/src/main/webapp/WEB-INF/views/video.jsp
@@ -1,5 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!doctype html>
+<<<<<<< Updated upstream
 <div class="youtubeVideo" style="display:inline-block; padding-top: 20px;">
 	<iframe id="video" width="350" height="230" frameborder="0" allowfullscreen></iframe>
+=======
+<<<<<<< HEAD
+<div style="display:inline-block; padding-top: 20px;">
+	<iframe id="video" width="400" height="250" frameborder="0" allowfullscreen></iframe>
+=======
+<div class="youtubeVideo" style="display:inline-block; padding-top: 20px;">
+	<iframe id="video" width="350" height="230" frameborder="0" allowfullscreen></iframe>
+>>>>>>> 5e5635d42212c89653e1de1ab0608f4ffb0949a9
+>>>>>>> Stashed changes
 </div>
