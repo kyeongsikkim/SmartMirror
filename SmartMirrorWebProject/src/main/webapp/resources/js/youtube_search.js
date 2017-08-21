2283f370395d8b80cb874549621311e7210bb635
Dahye Lee <PrayAnne@idahyeui-MacBook-Pro.local>
2017-08-21 15:57:19 +0900

    8/21 youtube

--- SmartMirrorWebProject/src/main/webapp/resources/js/youtube_search.js
+++ SmartMirrorWebProject/src/main/webapp/resources/js/youtube_search.js
@@ -1,13 +1,11 @@
-//var playParameter;
-var youtubeList;
+var playParameter;
 
 // Search
 function youtubeSearch(command) {
-	// playParameter = command;
+	playParameter = command;
 	$.ajax({
 		url: "https://www.googleapis.com/youtube/v3/search?part=snippet&order=videoCount&maxResults=3&q="+command+"&type=video&videoCaption=closedCaption&key=AIzaSyDSBgGx_mh_ZzOA14ftGFo2yEEzjBEMQiM",
 		success: function(data) {
-			youtubeList = data;
 			document.getElementById("video1").src = '//www.youtube.com/embed/'+data.items[0].id.videoId+'?rel=0&vq=small&autoplay=0';
 			$('#title1').html(data.items[0].snippet.title);
 			document.getElementById("video2").src = '//www.youtube.com/embed/'+data.items[1].id.videoId+'?rel=0&vq=small&autoplay=0';
@@ -20,5 +18,11 @@ function youtubeSearch(command) {
 
 //Play
 function playVideo(number) {
-	document.getElementById("video").src = '//www.youtube.com/embed/'+youtubeList.items[number].id.videoId+'?rel=0&vq=small&autoplay=1';
+	$.ajax({
+		url: "https://www.googleapis.com/youtube/v3/search?part=snippet&order=videoCount&maxResults=3&q="+playParameter+"&type=video&videoCaption=closedCaption&key=AIzaSyDSBgGx_mh_ZzOA14ftGFo2yEEzjBEMQiM",
+		success: function(data) {
+			console.log(data.items[number].id.videoId);
+			document.getElementById("video").src = '//www.youtube.com/embed/'+data.items[number].id.videoId+'?rel=0&vq=small&autoplay=1';
+		}
+	});
 }
